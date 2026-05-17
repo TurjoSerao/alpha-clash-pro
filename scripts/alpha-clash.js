f515857c0b072b3aeb4b404962ec26aa4586c956
devserao <seraotourjo@gmail.com>
2026-05-18 00:13:18 +0600

    added keybord functionality

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,7 +11,30 @@ function continueGame (){
     // Set Background Color 
     setBackgroundColor(alphabet);
 }
-// remove bg color    
+
+// keyboard keyUp Function
+
+function handelKeyboardKeyUp (event){
+    const playerPressed = event.key;
+    console.log(playerPressed);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabets = currentAlphabet.toLowerCase();
+    console.log(expectedAlphabets);
+
+    if(playerPressed === expectedAlphabets){
+        console.log('you get a point');
+        removeBackgroundColor(expectedAlphabets)
+        continueGame()
+    }
+    else{
+        console.log('you lost');
+    }
+    
+}
+
+document.addEventListener('keyup', handelKeyboardKeyUp)
 
 function play(){
     hideElementById('home-screen')
